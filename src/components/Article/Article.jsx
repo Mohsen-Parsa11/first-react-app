69cfd93fed10ddd78b04eb4a0fc8366aac61f746
Mohsen Parsa <mohsenp249@gmail.com>
2024-12-16 23:20:45 +0430

    home template developed

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,12 +1,13 @@
 import styled from "./article.module.css"
 import article from "./../../assets/images/people2.jpg"
 
-function Article () {
+function Article (props) {
+    // console.log(props.article.title)
     return (
         <div className={styled.articleWrapper}>
             <img src={article} alt="photo" />
-            <h5>برنده قهرمان ملی</h5>
-            <p>خواندن در ۵ دقیقه</p>
+            <h5>{ props.article.title}</h5>
+        <p>خواندن در {props.article.readingTime} دقیقه</p>
         </div>
     );
 }
